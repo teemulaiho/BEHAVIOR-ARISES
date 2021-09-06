088d911530008724c8931e39fc77623fab7ad185
teemulaiho <teemu.laiho92@gmail.com>
2021-09-06 17:47:00 +0300

    added menu

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    
+    public List<GameObject> moshbots;
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(this);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void LoadSceneMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void LoadSceneMoshBotViewer()
+    {
+        SceneManager.LoadScene("Moshbotviewer");
+    }
+}
